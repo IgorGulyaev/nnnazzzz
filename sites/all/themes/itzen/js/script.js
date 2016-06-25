395aeaad59b56338abd4492dccad9bc215ebcf5b
Igor Gulyaev <Igor Gulyaev>
2016-06-25 20:53:11 +0300

    Master Slider implemented

---
+++ sites/all/themes/itzen/js/script.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function ($) {
+
+    $('.view-meet-our-team .view-content').addClass('master-slider').attr('id', 'team_slider');
+    var portSlider = new MasterSlider();
+    var portSlideWidth = $(window).width();
+    portSlider.setup('team_slider' , {
+        width: portSlideWidth,
+        height:350,
+        space:200,
+        fullwidth:true,
+        loop:true,
+        view:'flow'
+    });
+    portSlider.control('team_arrows');
+
+});
