8db47a42bd8dc3bf3f414873a1873439b9a27894
IgorGulyaev <i.gulyaev88@gmail.com>
2016-09-08 12:17:07 +0300

    TwinMax scroll plugin added

--- sites/all/themes/itzen/js/script.js
+++ sites/all/themes/itzen/js/script.js
@@ -83,6 +83,28 @@ jQuery(document).ready(function ($) {
 
     new WOW().init();
 
+    var $window = $(window);		//Window object
+
+    var scrollTime = 0.6;			//Scroll time
+    var scrollDistance = 200;		//Distance. Use smaller value for shorter scroll and greater value for longer scroll
+
+    $window.on("mousewheel DOMMouseScroll", function (event) {
+
+        event.preventDefault();
+
+        var delta = event.originalEvent.wheelDelta / 120 || -event.originalEvent.detail / 3;
+        var scrollTop = $window.scrollTop();
+        var finalScroll = scrollTop - parseInt(delta * scrollDistance);
+
+        TweenMax.to($window, scrollTime, {
+            scrollTo: {y: finalScroll, autoKill: true},
+            ease: Power1.easeOut,
+            autoKill: true,
+            overwrite: 5
+        });
+
+    });
+
     $('.view-how-we-work-tabs .nav > li').each(function () {
         $(this).find('a').attr('data-slide-index', $(this).index()).wrapInner('<span class="label-text"></span>');
         var labelNumber = $(this).index() + 1;
